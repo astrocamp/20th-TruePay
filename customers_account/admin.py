4afffe88064f8b5145f3cfa8dce846cc8a7d29a6
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-08-19 13:41:42 +0800

    feat:消費者的註冊登入登出功能

---
+++ customers_account/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'account_status', 'email_verified', 'created_at', 'last_login']
+    list_filter = ['account_status', 'email_verified', 'created_at']
+    search_fields = ['name', 'email', 'phone', 'id_number']
+    readonly_fields = ['created_at', 'last_login', 'login_failed_count', 'reset_password_token']
+    
+    fieldsets = (
+        ('基本資料', {
+            'fields': ('email', 'name', 'id_number', 'birth_date', 'phone')
+        }),
+        ('帳號狀態', {
+            'fields': ('account_status', 'email_verified', 'login_failed_count')
+        }),
+        ('系統資訊', {
+            'fields': ('created_at', 'last_login', 'reset_password_token'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_delete_permission(self, request, obj=None):
+        # 可以根據需求限制刪除權限
+        return request.user.is_superuser
