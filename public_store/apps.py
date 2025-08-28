f5cba211f8c99055d6421e808461e7aab541f689
ethan <miso1024miso@gmail.com>
2025-08-28 12:38:41 +0800

    新增public_store模組 管理消費者看到畫面 包含麵包屑功能

---
+++ public_store/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PublicStoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'public_store'
+    verbose_name = '公開商店頁面'
