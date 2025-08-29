10ce74b5e52631f462dad356d7426c3544deff10
Tim <timbaby405@gmail.com>
2025-08-29 12:29:43 +0800

    增加語法

--- merchant_account/views.py
+++ merchant_account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.core.paginator import Paginator
 from truepay.decorators import no_cache_required, shop_required
