0916f7e99b737d8815e18aeb0eb56dc1e9bbb20b
ethan <miso1024miso@gmail.com>
2025-09-03 15:27:41 +0800

    首頁文字&卡片優化

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: [
     "./templates/**/*.html",
-    "./pages/templates/**/*.html", 
+    "./pages/templates/**/*.html",
+    "./pages/**/*.html",
+    "./merchant_account/**/*.html",
+    "./customers_account/**/*.html", 
+    "./merchant_marketplace/**/*.html",
+    "./public_store/**/*.html",
     "./src/**/*.{js,ts}",
     "./static/**/*.{js,ts}",
   ],
@@ -17,6 +22,14 @@ export default {
           grayDark: "#333333",  // 深灰
           grayLight: "#F5F5F7", // 淺灰
           grayBorder: "#D2D2D7" // 分隔線灰
+        },
+        cis: {
+          primary: "#003d82",     // CIS 主色 - 深藍
+          primaryHover: "#002a5c", // CIS 主色 Hover
+          secondary: "#00a651",   // CIS 次要色 - 綠色
+          secondaryHover: "#007d3c", // CIS 次要色 Hover
+          accent: "#ff6b35",      // CIS 強調色 - 橙色
+          accentHover: "#e5572e", // CIS 強調色 Hover
         },
       },
     },
