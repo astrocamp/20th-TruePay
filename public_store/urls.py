d57c8b8d13e70a73d3270a2b8bd7222d9c418a1b
Tim <timbaby405@gmail.com>
2025-09-11 12:54:19 +0800

    修復開啟收款頁面連結問題

--- public_store/urls.py
+++ public_store/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "public_store"
 
 urlpatterns = [
-    path("", views.shop_overview, name="shop_overview_custom"),
-    path("pay/<int:id>/", views.payment_page, name="payment_page_custom"),
+    path("", views.shop_overview, name="shop_overview"),
+    path("pay/<int:id>/", views.payment_page, name="payment_page"),
     path("<slug:subdomain>/", views.shop_overview, name="shop_overview"),
     path("<slug:subdomain>/pay/<int:id>/", views.payment_page, name="payment_page"),
 ]
