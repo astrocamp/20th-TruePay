c8a18aca327b258a8d59553e1742ef4e9e68c778
marcpikachu <forcervp0206@gmail.com>
2025-09-11 16:57:32 +0800

    refactor:將 html5-qrcode 從 CDN 改為透過 npm 管理並整合到 vite 建置流程中

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,6 @@
 import Alpine from 'alpinejs'
 import htmx from 'htmx.org';
+import { Html5Qrcode } from 'html5-qrcode';
 import '../styles/app.css'
 import './functions.js'
 import jsQR from 'jsqr'
@@ -7,4 +8,5 @@ import jsQR from 'jsqr'
 window.Alpine = Alpine
 window.htmx = htmx
 window.jsQR = jsQR
+window.Html5Qrcode = Html5Qrcode
 Alpine.start()
