b9a93275fe1e9fb3a0113afa85f1ee9f2ed57e0f
WENLIN-CHANG <magic8405cigam6@gmail.com>
2025-09-24 09:49:45 +0800

    feat:加入隱私權、使用條款

--- pages/urls.py
+++ pages/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("marketplace/", views.marketplace, name="marketplace"),
     path("selectrole/", views.selectrole, name="selectrole"),
+    path("terms/", views.terms, name="terms"),
+    path("privacy/", views.privacy, name="privacy"),
 ]
