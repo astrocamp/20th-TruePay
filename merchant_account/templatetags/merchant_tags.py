8ebcfb975e81251c161f717a0a7489dbda2086ad
Tim <timbaby405@gmail.com>
2025-09-25 16:13:26 +0800

    網站整體樣式調整

--- merchant_account/templatetags/merchant_tags.py
+++ merchant_account/templatetags/merchant_tags.py
@@ -17,6 +17,30 @@ def get_current_merchant(context):
     return "/merchant/login/"
 
 
+@register.simple_tag(takes_context=True)
+def get_page_merchant(context):
+    """取得當前頁面的商家資料"""
+    request = context["request"]
+
+    # 先檢查模板context中是否已有商家資料
+    if 'merchant' in context:
+        return context['merchant']
+    elif 'product' in context and hasattr(context['product'], 'merchant'):
+        return context['product'].merchant
+
+    # 如果沒有，嘗試從URL路徑中取得subdomain
+    path_parts = request.path.strip('/').split('/')
+    if len(path_parts) >= 2 and path_parts[0] == 'shop':
+        subdomain = path_parts[1]
+        try:
+            merchant = Merchant.objects.get(subdomain=subdomain)
+            return merchant
+        except Merchant.DoesNotExist:
+            pass
+
+    return None
+
+
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
